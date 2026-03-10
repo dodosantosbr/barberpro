aad7d21ac87e71b66d827875cd4b96a3122ab79e
Douglas Sales <sallesard@gmail.com>
2026-03-10 20:10:50 -0300

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://barberpro-sand.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
