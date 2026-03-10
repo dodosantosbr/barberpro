7bbb99cd2a516716cfdde5778701254010d9a910
Douglas Sales <sallesard@gmail.com>
2026-03-09 22:38:13 -0300

    fix port railway

--- backend/src/server.js
+++ backend/src/server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 // ============================
 // 🚀 Start server
 // ============================
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 API BarberPro rodando na porta ${PORT}`);
