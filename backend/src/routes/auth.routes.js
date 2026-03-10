b7fe776f10f06514553368db123c1e1ac20a1145
Douglas Sales <sallesard@gmail.com>
2026-03-10 20:07:50 -0300

    fix:nodemailer

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -142,7 +142,9 @@ router.post("/forgot-password", async (req, res) => {
     });
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
